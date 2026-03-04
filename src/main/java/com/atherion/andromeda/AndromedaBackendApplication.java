c6c6d6742f53709909b78d68fa5da9a5696e6743
SantiQ <a01571222@itesm.mx>
2026-03-04 10:30:39 -0600

    Update to project settings and env

---
+++ src/main/java/com/atherion/andromeda/AndromedaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.atherion.andromeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndromedaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndromedaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atherion/andromeda/AndromedaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atherion.andromeda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndromedaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
