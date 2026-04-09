c7f36ed561990d3aadf789388945c47e3f41e6ab
SantiQ <a01571222@itesm.mx>
2026-04-08 18:14:25 -0600

    DBtests ignored and new CI pipeline.

--- src/test/java/com/atherion/andromeda/AndromedaBackendApplicationTests.java
+++ src/test/java/com/atherion/andromeda/AndromedaBackendApplicationTests.java
@@ -1,13 +1,19 @@
 package com.atherion.andromeda;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 class AndromedaBackendApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
